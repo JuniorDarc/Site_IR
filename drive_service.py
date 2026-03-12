bde300a8fa149b6df3b7938826b86c57aa9ce4ca
Magster_f <edjuniorslv77@gmail.com>
2026-03-12 19:53:56 -0300

    Update drive_service.py

--- drive_service.py
+++ drive_service.py
@@ -1,12 +1,18 @@
+import os
+import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-SERVICE_ACCOUNT_FILE = "buscarpdfs-ce7c8346aac6.json"
 
 def get_drive_service():
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
+    credentials_json = os.environ.get("GOOGLE_CREDENTIALS")
+
+    credentials_dict = json.loads(credentials_json)
+
+    credentials = service_account.Credentials.from_service_account_info(
+        credentials_dict,
         scopes=SCOPES
     )
+
     return build("drive", "v3", credentials=credentials)
